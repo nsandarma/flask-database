c431630599269ecc6c670ad547a7d37940653a7b
nsandarma <nsandarma@gmail.com>
2021-10-16 01:01:21 +0700

    Add Project Flask Crud

---
+++ app/modules/controller.py
@@ -0,0 +1,62 @@
+from flask import render_template, request, redirect
+from app import app
+from .models import db, Mahasiswa
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        nim = request.form['nim']
+        jurusan = request.form['jurusan']
+        try:
+            mhs = Mahasiswa(nim=nim, name=name,jurusan=jurusan)
+            db.session.add(mhs)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add data.")
+            print(e)
+    listMhs = Mahasiswa.query.all()
+    print(listMhs)
+    # return redirect("/")
+    return render_template("home.html",data=enumerate(listMhs,0))
+
+@app.route('/form-update/<int:id>')
+def updateForm(id):
+    mhs = Mahasiswa.query.filter_by(id=id).first()
+    return render_template("form-update.html", data=mhs)
+    # return "hello world"
+
+@app.route('/form-update', methods=['POST'])
+def update():
+    if request.method == 'POST':
+        id = request.form['id']
+        nim = request.form['nim']
+        name = request.form['name']
+        jurusan = request.form['jurusan']
+        try:
+            mhs = Mahasiswa.query.filter_by(id=id).first()
+            mhs.name = name
+            mhs.nim = nim
+            mhs.jurusan = jurusan
+            db.session.commit()
+        except Exception as e:
+            print("Failed to update data")
+            print(e)
+        return redirect("/")
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    try:
+        mhs = Mahasiswa.query.filter_by(id=id).first()
+        db.session.delete(mhs)
+        db.session.commit()
+    except Exception as e:
+        print("Failed delete mahasiswa")
+        print(e)
+    return redirect("/")
+
+
+
+
+
+
---
+++ app/modules/rest_api.py
@@ -0,0 +1,68 @@
+from flask_restful import Resource,Api,reqparse
+from app import app
+from .models import db, Mahasiswa
+from flask_marshmallow import Marshmallow # new
+from flask import request
+
+ma = Marshmallow(app) # new
+
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('nim')
+parser.add_argument('jurusan')
+parser.add_argument('id')
+
+
+class PostSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "nim","jurusan")
+        model = Mahasiswa
+
+post_schema = PostSchema()
+posts_schema = PostSchema(many=True)
+
+
+api = Api(app)
+
+class Data(Resource):
+    def get(self):
+        listMhs = Mahasiswa.query.all()
+        return {'data':posts_schema.dump(listMhs)}
+    def post(self):
+        args = parser.parse_args()
+        nim = args['nim']
+        jurusan = args['jurusan']
+        name = args['name']
+        try:
+            mhs = Mahasiswa(name=name,nim=nim,jurusan=jurusan)
+            db.session.add(mhs)
+            db.session.commit()
+            return {'status':'True',"data":[name,nim,jurusan]}
+        except Exception as e:
+            return {"status":"failed"}
+    def delete(self):
+        args = parser.parse_args()
+
+        id = args['id']
+
+        try:
+            mhs = Mahasiswa.query.filter_by(id=id).first()
+            db.session.delete(mhs)
+            db.session.commit()
+            return {'status':'deleted'}
+        except Exception as e:
+            return {'status':"Error"}
+        
+
+class GetDatabyid(Resource):
+    def get(self,nim):
+        mhs = Mahasiswa.query.filter_by(nim=nim).first()
+        return {'data':post_schema.dump(mhs)}
+
+        
+
+
+api.add_resource(Data,'/api')
+api.add_resource(GetDatabyid,'/api/<nim>')
+
+
